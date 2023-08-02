3b57d9ba92ee0703a050d1265420ee2a902488af
sweetlyra01 <binibingsweetlyra@gmail.com>
2023-08-02 14:17:30 +0800

    first commit

--- script/main.js
+++ script/main.js
@@ -5,7 +5,7 @@ let Numbers = ""
 function checkNumbers() {
         if (inputNumber.value%2 !=0) {
             display.innerHTML = "you entered an ODD number"
-        }else if (inputNumber.value%2 === 0 && inputNumber.value%2 >0 ){
+        }else if (inputNumber.value%2 === 0 ){
             display.innerHTML = "you entered an EVEN number"
         } else {
             display.innerHTML = "please type a number"
